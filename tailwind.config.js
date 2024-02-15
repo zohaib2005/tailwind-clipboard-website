8743fee79881c2eb15ad1e4f1f210dc3d6120a2d
zohaib <zohaibshafiq2005@gmail.com>
2024-02-15 18:43:56 +0500

    fonts and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "786px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      extend: {
+        colors: {
+          strongCyan: "hsl(171, 66%, 44%)",
+          lightBlue: "hsl(233, 100%, 69%)",
+          darkGrayishBlue: "hsl(210, 10%, 33%)",
+          grayishBlue: "hsl(201, 11%, 66%)",
+        },
+        fontFamily: {
+          sans: ["Bai Jamjuree", "sans-serif"],
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
