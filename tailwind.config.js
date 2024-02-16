f0cc58e7c0a38fdf11f9c339bb350ff401aeddde
zohaib <zohaibshafiq2005@gmail.com>
2024-02-16 21:28:37 +0500

    clipboard header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
   theme: {
@@ -9,18 +8,21 @@ module.exports = {
         lg: "976px",
         xl: "1440px",
       },
-      extend: {
-        colors: {
-          strongCyan: "hsl(171, 66%, 44%)",
-          lightBlue: "hsl(233, 100%, 69%)",
-          darkGrayishBlue: "hsl(210, 10%, 33%)",
-          grayishBlue: "hsl(201, 11%, 66%)",
-        },
-        fontFamily: {
-          sans: ["Bai Jamjuree", "sans-serif"],
-        },
+      colors: {
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
+      fontFamily: {
+        sans: ["Bai Jamjuree", "sans-serif"],
       },
     },
   },
   plugins: [],
+  safelist: [
+    {
+      pattern: /(bg|text|border)-darkGrayishBlue/,
+    },
+  ],
 };
